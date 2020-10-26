4a4485b6a49bc524b66a5ad65939125ac79b0c96
MGeraud <geraud.mon@gmail.com>
2020-10-26 18:06:07 +0100

    Refactor batchController to send proper List of loans

--- src/main/java/com/geraud/ocr_loan_api/controllers/BatchController.java
+++ src/main/java/com/geraud/ocr_loan_api/controllers/BatchController.java
@@ -3,6 +3,8 @@ package com.geraud.ocr_loan_api.controllers;
 import com.geraud.ocr_loan_api.domain.Loan;
 import com.geraud.ocr_loan_api.services.BatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,8 @@ public class BatchController {
     BatchService batchService;
 
     @GetMapping("/batch")
-    public List<Loan> batchDtoList(@RequestParam("date") String date,
-                                   @RequestParam("refresh") int refresh){
-        return batchService.findTitleAndEmail(LocalDate.parse(date), refresh);
+    public ResponseEntity<List<Loan>> batchDtoList(@RequestParam("date") String date,
+                                                  @RequestParam("refresh") int refresh){
+        return new ResponseEntity<>(batchService.findTitleAndEmail(LocalDate.parse(date), refresh), HttpStatus.OK);
     }
 }
